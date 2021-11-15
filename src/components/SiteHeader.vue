f461b8e9aa83f379457a273c4e290977cbfb0abb
rota99C <rotaboolean@gmail.com>
2021-11-15 16:39:04 +0100

    create component header stylised with scss

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="header">
+    <img src="../img/Spotify_logo_without_text.svg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#header {
+  height: 70px;
+  width: 100%;
+  background-color: #2e3a46;
+  padding: 15px 30px;
+  img {
+    height: 100%;
+  }
+}
+</style>
