0adfceb33df98f106b21bb92d52ac45c665a861c
rota99C <rotaboolean@gmail.com>
2021-11-16 17:15:58 +0100

    creato select in modo dinamico

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -2,17 +2,23 @@
   <div class="container">
     <label for="genres">filtra per genere musicale </label>
     <select v-model="selection" @change="changeSelection">
-      <option value="tutti">Tutti</option>
+      <option v-for="(genre, i) in genres" :key="i" :value="genre">
+        {{ genre }}
+      </option>
+      <!-- <option value="tutti">Tutti</option>
       <option value="rock">Rock</option>
       <option value="pop">Pop</option>
       <option value="jazz">Jazz</option>
-      <option value="Metal">Metal</option>
+      <option value="Metal">Metal</option> -->
     </select>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    genres: [],
+  },
   data() {
     return {
       selection: "",
